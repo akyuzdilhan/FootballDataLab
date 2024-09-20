c2f503299b4d95f18ce0da794af2ba4f2d784613
Ippo <dilhan.akyuz72@gmail.com>
2024-09-20 10:13:04 -0400

    Fix bug for filtered integers in table

--- plotly-dash/callbacks/player_analysis_callbacks.py
+++
@@ -1,1 +0,0 @@
-from layouts.player_analysis import get_columns_with_headers
--- plotly-dash/layouts/player_analysis.py
+++ plotly-dash/layouts/player_analysis.py
@@ -1,6 +1,5 @@
 import pandas as pd
-from dash import html, dash_table, dcc, Dash
-from dash.dash_table.Format import Format
+from dash import html, dash_table, dcc
 
 df_player_stats = pd.read_csv('../datasets/stats_player_mls.csv')
 flags_iso = pd.read_csv('../datasets/flags_iso.csv')
@@ -12,13 +11,15 @@ def add_flag_to_nation(row):
     flag_url = flags_dict.get(row['nation'], '')
     if flag_url:
         return f"![flag]({flag_url}) {row['nation']}"
+    else:
+        return row['nation'] if pd.notna(row['nation']) else ''
 
 # Add flag images to the nation column
-df_player_stats['nation'] = df_player_stats.apply(lambda row: add_flag_to_nation(row) if pd.notna(row['nation']) else '', axis=1)
+df_player_stats['nation'] = df_player_stats.apply(lambda row: add_flag_to_nation(row), axis=1)
 
 # Column groups and headers
 column_groups = {
-    "": ['team', 'player', 'nation', 'pos', 'age', 'born'],
+    "": ['Team', 'player', 'nation', 'pos', 'age', 'born'],
     "Playing Time": ['MP', 'Starts', 'Min', '90s'],
     "Performance": ['Gls', 'Ast', 'G+A', 'G-PK', 'PK', 'PKatt', 'CrdY', 'CrdR'],
     "Expected": ['xG', 'npxG', 'xAG', 'npxG+xAG'],
@@ -31,16 +32,20 @@ def get_columns_with_headers(groups):
     for group, cols in groups.items():
         for col in cols:
             display_name = col.replace("/90", "") if group == "Per 90 Minutes" else col
-            col_type = "numeric" if col not in ['team', 'player', 'nation', 'pos', 'age', 'born'] else "text"
-            presentation = 'markdown' if col == 'nation' else 'input'
-            columns.append({
+            col_type = "numeric" if col not in ['Team', 'player', 'nation', 'pos'] else "text"
+            
+            column = {
                 "name": [group, display_name], 
                 "id": col, 
                 "type": col_type,
-                "format": Format(nully=''), 
-                "presentation": presentation
-            })
+            }
+            
+            if col == 'nation':
+                column['presentation'] = 'markdown'
+            
+            columns.append(column)
     return columns
+
 df_player_stats_display = df_player_stats.copy()
 
 # Distinguishing separator for column groups
@@ -75,25 +80,41 @@ layout = html.Div([
             data=df_player_stats_display.to_dict('records'),
 
             style_table={'overflowX': 'auto', 'width': '100%', 'minWidth': '100%'},
-            filter_options={'height': '30px', 'fontSize': '20px', "placeholder_text": "filter", "case": "insensitive"},
-            style_cell={'textAlign': 'center', 'whiteSpace': 'nowrap', 'fontSize': '6px', 'border': '1px solid #d3d3d3'},
-            style_header={'backgroundColor': '#F8F5F0', 'fontWeight': 'bold', 'fontSize': '14px', 'border': '1px solid #d3d3d3', 'whiteSpace': 'nowrap'},
+            style_cell={
+                'textAlign': 'center',
+                'whiteSpace': 'nowrap',
+                'fontSize': '6px',
+                'border': '1px solid #d3d3d3'
+            },
+            style_header={
+                'backgroundColor': '#F8F5F0',
+                'fontWeight': 'bold',
+                'fontSize': '14px',
+                'border': '1px solid #d3d3d3',
+                'whiteSpace': 'nowrap'
+            },
+            style_filter={'height': '30px', 'fontSize': '14px'},
 
+            # DataTable options
             page_size=30,
             filter_action="native",
             sort_action='native',
             sort_mode="multi",
 
+            # Merge headers and apply styles
             merge_duplicate_headers=True,
             style_header_conditional=header_style,
             style_data_conditional=cell_style,
             style_filter_conditional=filter_style,
-            tooltip_header={ # TODO apply the metrics from utils
+
+            filter_options={'placeholder_text': 'Filter'}, # 'case': 'insensitive', → Option deleted because it creates a filtering bug for integers
+
+            tooltip_header={
                 'nation': 'Nationality',
                 'pos': 'Position',
                 'born': 'Year Born',
                 'MP': 'Matches Played',
-                'Starts': 'Game or games started by player',
+                'Starts': 'Games Started by Player',
                 'Min': 'Minutes Played',
                 'Gls': 'Goals',
                 'Ast': 'Assists',
@@ -103,23 +124,23 @@ layout = html.Div([
                 'PKatt': 'Penalty Kicks Attempted',
                 'CrdY': 'Yellow Cards',
                 'CrdR': 'Red Cards',
-                'xG': 'xG: Expected Goals',
-                'npxG': 'npxG: Non-Penalty xG',
-                'xAG': 'xAG: Exp. Assisted Goals',
-                'npxG+xAG': 'Non-Penalty Expected Goals + Assists',
+                'xG': 'Expected Goals',
+                'npxG': 'Non-Penalty Expected Goals',
+                'xAG': 'Expected Assisted Goals',
+                'npxG+xAG': 'Non-Penalty Expected Goals + Assisted Goals',
                 'PrgC': 'Progressive Carries',
                 'PrgP': 'Progressive Passes',
-                'PrgR': 'Progressive Passes Rec',
+                'PrgR': 'Progressive Passes Received',
                 'Gls/90': 'Goals per 90 minutes',
                 'Ast/90': 'Assists per 90 minutes',
                 'G+A/90': 'Goals + Assists per 90 minutes',
                 'G-PK/90': 'Non-Penalty Goals per 90 minutes',
-                'G+A-PK/90': 'Goals plus Assists minus Penalty Kicks made per 90 minutes',
+                'G+A-PK/90': 'Goals + Assists minus Penalty Kicks per 90 minutes',
                 'xG/90': 'Expected Goals per 90 minutes',
                 'xAG/90': 'Expected Assisted Goals per 90 minutes',
-                'xG+xAG/90': 'Expected Goals plus Assisted Goals per 90 minutes',
+                'xG+xAG/90': 'Expected Goals + Assisted Goals per 90 minutes',
                 'npxG/90': 'Non-Penalty Expected Goals per 90 minutes',
-                'npxG+xAG/90': 'Non-Penalty Expected Goals plus Assisted Goals per 90 minutes'
+                'npxG+xAG/90': 'Non-Penalty Expected Goals + Assisted Goals per 90 minutes'
             },
         )
     ], style={'width': '100%'}),
